387d1e364e00d51fc4de5a743567dff4bc3deb8c
Bryan Cole <Bryan.Cole@sjsu.edu>
2018-01-27 16:00:59 -0800

    Lab 1 written. bugs still need to be found/added for bug finding lab

---
+++ lib/lab01/inc/expressionstream.h
@@ -0,0 +1,30 @@
+#ifndef CMPE126S18_LABS_STSTRING_H
+#define CMPE126S18_LABS_STSTRING_H
+
+
+#include <string>
+
+class expressionstream {
+private:
+    std::string buffer;
+    std::string::iterator pos_next;
+    std::string::iterator next_int;
+    std::string::iterator next_op;
+
+public:
+    explicit expressionstream(const std::string &string_in);
+
+    std::string str() const;
+    void str (const std::string& s);
+
+    bool integer_is_next();
+    bool operator_is_next();
+    bool end_is_next();
+
+    std::string get_next_int();
+    std::string get_next_op();
+
+};
+
+
+#endif //CMPE126S18_LABS_STSTRING_H
---
+++ lib/lab01/src/expressionstream.cpp
@@ -0,0 +1,73 @@
+#include "expressionstream.h"
+
+bool is_operator(char c);
+bool is_digit(char c);
+
+expressionstream::expressionstream(const std::string &string_in) : buffer(string_in) {
+    pos_next = buffer.begin();
+    next_int= buffer.begin();
+    next_op = buffer.begin();
+}
+
+std::string expressionstream::str() const {
+    return buffer;
+}
+
+void expressionstream::str(const std::string &s) {
+    buffer = s;
+}
+
+bool expressionstream::integer_is_next() {
+    return next_int==pos_next;
+}
+
+bool expressionstream::operator_is_next() {
+    return next_op==pos_next;
+}
+
+bool expressionstream::end_is_next() {
+    return pos_next == buffer.end();
+}
+
+std::string expressionstream::get_next_int() {
+    bool is_neg=false, op_found=false;
+    std::string::iterator int_end;
+    std::string integer;
+
+    while(next_int != buffer.end() && is_operator(*next_int)){ //align next_int with next digit in buffer
+        if(*next_int!=' ') {
+            if (op_found) {
+                if (*next_int == '-') {
+                    is_neg = true;
+                } else {
+                    throw "String contains errors";//could be more than one operator side by side
+                }
+            } else {
+                op_found = true;
+            }
+        }
+        next_int++;
+    }
+    int_end = next_int +1;
+
+    while(int_end != buffer.end() && is_digit(*int_end)) int_end++;
+
+    integer = std::string(next_int,int_end);
+    next_int=int_end;
+
+    if(is_neg) integer = '-' + integer;
+    return integer;
+}
+
+std::string expressionstream::get_next_op() {
+    while(next_op != buffer.end() && !is_operator(*next_op))next_op++;
+    return std::string(next_op,next_op++);
+}
+
+bool is_digit(char c){
+    return (c >= '0' && c <= '9');
+}
+
+bool is_operator(char c){
+    return (c=='+' || c=='-' || c=='*' || c=='/' || c=='(' || c==')' || c=='^');
+}
