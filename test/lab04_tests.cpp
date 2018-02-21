041d448298aa3ae4eadd29c0bb90d26a8a3a0e10
alex <alex.norell@gmail.com>
2018-02-20 22:30:37 -0800

    Added lab4 tests file and updated test cmake

---
+++ test/lab04_tests.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "fifo.h"
+#include "lifo.h"
+#include "calculator.h"
+
+class Lab04Fixture : public ::testing::Test {
+
+protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+
+public:
+};
