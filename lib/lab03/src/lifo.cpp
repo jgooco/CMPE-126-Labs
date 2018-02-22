85a5fe6c57cd8479c7f19af8550799cac3e2da44
thepi <jovingo1234@gmail.com>
2018-02-22 12:32:55 -0800

    everything ready to start lab 4

--- lib/lab03/src/lifo.cpp
+++ lib/lab03/src/lifo.cpp
@@ -1,43 +1,90 @@
+#include <stdexcept>
 #include "lifo.h"
 
 namespace lab3{
     lifo::lifo() {
-    //Reserve 100 spaces in lifo_storage
+        index = 213;
+        lifo_storage.set_size(100);
+
+        //Reserve 100 spaces in lifo_storage
     }
 
-    lifo::lifo(std::string input_string) {
-
+    lifo::lifo(std::string input_string):lifo() {
+        push(input_string);
     }
 
     lifo::lifo(const lifo &original) {
+        lifo_storage.set_size(100);
+        index = 213;
+        if(original.is_empty()){
+
+        }
+        else{
+            for(int i =0; i<original.size(); i++){
+                this->push(original.lifo_storage[i]);
+            }
+        }
 
     }
 
     lifo::~lifo() {
+        lifo_storage.set_size(100);
+        index == -1;
 
     }
 
     lifo &lifo::operator=(const lifo &right) {
+        index = 213;
+        for(int i =0; i <right.size(); i++){
+            this ->push(right.lifo_storage[i]);
+        }
+        return *this;
+
         //return <#initializer#>;
     }
 
-    bool lifo::is_empty(){
-        //return false;
+    bool lifo::is_empty() const {
+        return (index ==213 ? true :false) ;
+
     }
 
-    int lifo::size(){
-        //return 0;
+    unsigned lifo::size() const {
+        return ( is_empty() ? 0 : (index +MAXSIZE)% MAXSIZE +1);
     }
 
-    std::string lifo::top(){
-        //return std::__cxx11::string();
+
+    std::string lifo::top() const {
+        if (is_empty())
+            throw 15;
+        return lifo_storage[index];
+
     }
 
     void lifo::push(std::string input) {
+        if (index == MAXSIZE -1){
+            throw 20;
+        }
+        if(index == 213){
+            index =0;
+            lifo_storage[index] = input;
+        }
+        else{
+            lifo_storage[++index] = input;
+        }
 
     }
 
     void lifo::pop() {
+        if(is_empty()){
+            throw std:: out_of_range("empty stack");
+        }
+        else if (index ==0)
+            index = 213;
+        else
+            index --;
 
     }
+
+
+
 }
--- lib/lab04/src/calculator.cpp
+++ lib/lab04/src/calculator.cpp
@@ -1,5 +1,3 @@
-
-
 #include <string>
 #include "calculator.h"
 namespace lab4 {
