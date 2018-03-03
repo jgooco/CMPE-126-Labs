f52f0793488d37823344d7963318d35ce0ef5d23
alex <alex.norell@gmail.com>
2018-03-02 20:42:53 -0800

    Added lab5 test file

---
+++ test/lab05_tests.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "queue.h"
+#include "stack.h"
+#include "fancy_calculator.h"
+
+class Lab05Fixture : public ::testing::Test {
+
+protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+
+public:
+};
