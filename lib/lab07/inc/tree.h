6ebc7bc41d5534460f0857715729f97e418cc23b
alex <alex.norell@gmail.com>
2018-04-03 01:07:16 -0700

    Initial framework for lab7

---
+++ lib/lab07/inc/tree.h
@@ -0,0 +1,10 @@
+#ifndef CMPE126S18_LABS_TREE_H
+#define CMPE126S18_LABS_TREE_H
+#include "node.h"
+
+class tree {
+
+};
+
+
+#endif //CMPE126S18_LABS_TREE_H
---
+++ test/lab07_tests.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+#include "tree.h"
+
+class Lab07Fixture : public ::testing::Test {
+
+protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+
+public:
+};
