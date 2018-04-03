a6d9aecba2c96702a3ba82709340d0387e74b72f
alex <alex.norell@gmail.com>
2018-04-03 01:18:18 -0700

    Node Implemented

--- lib/lab07/inc/node.h
+++ lib/lab07/inc/node.h
@@ -2,3 +2,13 @@
 #define CMPE126S18_LABS_NODE_H
 
 #endif //CMPE126S18_LABS_NODE_H
+namespace lab7{
+    class node{
+    public:
+        node *left, *right;  // Pointers to the left and right children
+        int data;           // Integer that is stored in the tree
+        unsigned frequency; // Frequency of that integer in the tree
+
+        explicit node(int input_data) : data(input_data), frequency(1), left(nullptr), right(nullptr) {}
+    };
+}
