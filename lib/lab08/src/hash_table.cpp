d4814e00b7e481e1025e3ae5a1452f24b5f739d9
thepi <jovingo1234@gmail.com>
2018-05-20 21:29:30 -0700

    lab 8 code for hashing almost done

--- lib/lab08/src/hash_table.cpp
+++ lib/lab08/src/hash_table.cpp
@@ -23,6 +23,28 @@ namespace lab8{
     void hash_table::expand() {
         // Expand the hash table by a factor of 2 every time this function is called
 
+
+        //max_size = max_size * 2;
+
+        for(int i = 0; i < 16; i++){
+            if( max_size < PRIMES[i]){
+                max_size = PRIMES[i];
+                break;
+            }
+        }
+
+        //Create new empty table doubled
+        key_value *oldTable = hash_table_array;
+        hash_table_array = new key_value[max_size];
+
+        //Copy all the elements in the old table to the new table
+        int oldCurrentSize = current_size;
+        current_size = 0;
+
+        for(int i = 0; i < oldCurrentSize; i++){
+            insert(oldTable[i].key, oldTable[i].value);
+        }
+        delete [] oldTable;
     }
 
     hash_table::hash_table(char type) {
@@ -37,16 +59,41 @@ namespace lab8{
          *
          * Create a hash table with an initial size of 100
          */
+
+        probing = type;
+        max_size = PRIMES[0];
+        current_size = 0;
+        hash_table_array = new key_value[max_size];
+
     }
 
     hash_table::~hash_table() {
-
+        delete [] hash_table_array;
     }
 
+
     bool hash_table::insert(std::string key, int value) {
-        // Insert a key according to the defined probing technique
-        // If you run into an issue where you get an infinite collision loop,
-        //   figure out a way to get out of the loop.
+        if(in_table(key))
+            return false;
+        unsigned hash = (hash_1(key)) % max_size;
+        unsigned attempt = 1;
+
+        while(!hash_table_array[hash].key.empty() and hash_table_array[hash].key != key) {
+            if(probing == 'l')
+                hash++;
+            else if(probing == 'q')
+                hash = (hash + attempt*attempt);
+            else if(probing == 'd')
+                hash = (hash + attempt*hash_2(key));
+            attempt ++;
+            hash %= max_size;
+        }
+        if(hash_table_array[hash].key == "") {
+            current_size++;
+        }
+        hash_table_array[hash].key = key;
+        hash_table_array[hash].value = value;
+        if(current_size/max_size > 0.7) expand();
         return true;
     }
 
@@ -54,30 +101,95 @@ namespace lab8{
         // Checks to see if that key is in the table.
         // Use the specified probing technique
         // Keep collisions in mind
-        return true;
+        unsigned hash = hash_1(key) % max_size;
+        unsigned attempt = 1;
+        while(hash_table_array[hash].key != "") {
+            if(hash_table_array[hash].key == key)
+                return true;
+            if(probing == 'l')
+                hash ++;
+            else if(probing == 'q')
+                hash = (hash + attempt*attempt);
+            else if(probing == 'd')
+                hash = (hash + attempt * hash_2(key));
+            attempt ++;
+            hash %= max_size;
+        }
+        return false;
     }
 
     int hash_table::get(std::string key) {
         // Get the int value from the node that has key
         // Use the specified probing technique
-        return 0;
+        unsigned hash = hash_1(key) % max_size;
+        unsigned attempt = 1;
+        while(hash_table_array[hash].key != "") {
+            if(hash_table_array[hash].key == key)
+                return hash_table_array[hash].value;
+            if(probing == 'l')
+                hash ++;
+            else if(probing == 'q')
+                hash = (hash + attempt*attempt);
+            else if(probing == 'd')
+                hash = (hash + attempt * hash_2(key));
+            attempt ++;
+            hash %= max_size;
+        }
+        throw std::exception();
     }
 
     void hash_table::update(std::string key, int value){
         // Update a key in the hash table.
         // Keep collisions in mind
         // Use the specified probing technique
+
+        unsigned hash = hash_1(key) % max_size;
+        unsigned attempt = 1;
+        while(hash_table_array[hash].key != "") {
+            if(hash_table_array[hash].key == key) {
+                hash_table_array[hash].value = value;
+                return;
+            }
+            if(probing == 'l')
+                hash ++;
+            else if(probing == 'q')
+                hash = (hash + attempt*attempt);
+            else if(probing == 'd')
+                hash = (hash + attempt * hash_2(key));
+            attempt ++;
+            hash %= max_size;
+        }
+        insert(key, value);
     }
 
     void hash_table::remove(std::string key){
         // Remove an item from the hash table. Keep collisions in mind
         // Use the specified probing technique
         // If the item isn't in the table, do nothing.
+
+        unsigned hash = hash_1(key) % max_size;
+        unsigned attempt = 1;
+        while(hash_table_array[hash].key != "") {
+            if(hash_table_array[hash].key == key) {
+                current_size--;
+                hash_table_array[hash].key = "";
+            }
+            if(probing == 'l')
+                hash ++;
+            else if(probing == 'q')
+                hash = (hash + attempt*attempt);
+            else if(probing == 'd')
+                hash = (hash + attempt * hash_2(key));
+            attempt ++;
+            hash %= max_size;
+        }
+
     }
 
     std::string hash_table::to_string(){
         // Run through the entire array and create a string following this format. The {} brackets aren't part of the return
         // [{array location}]{key_value.key}:{key_value.int}
-    return std::string();
+
+        return std::string();
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+int func(int x);
 int main() {
-    std::cout << "Hello Thursday everyone!";
-    return 0;
+
+
+    std::cout <<func(2) << std:: endl;
+    std::cout <<func(3) << std:: endl;
+    std::cout <<func(4) << std:: endl;
+    std::cout <<func(5) << std:: endl;
+}
+
+int func(int x){
+    if(!x) return 1;
+    else if (!(x-1)) return 2;
+    else return func(x-1) + func(x-2);
+
 }
